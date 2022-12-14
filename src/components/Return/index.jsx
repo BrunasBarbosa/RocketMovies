2ef7cbb900651b499c9520873e7e34359403e203
Bruna Barbosa <brunas_barbosa@hotmail.com>
2022-12-14 13:11:29 -0300

    fix Return navigate

--- src/components/Return/index.jsx
+++ src/components/Return/index.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi';
 import { Container } from './styles';
 
-export function Return() {
+export function Return({ onClick, ...rest }) {
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
   return (
-    <Container to="/" >
+    <Container
+      onClick={handleBack}
+      {...rest}
+    >
       <FiArrowLeft />
       Voltar
     </Container>
