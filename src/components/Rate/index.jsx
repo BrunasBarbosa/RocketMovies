0244531abe642cb702b16c9bb881d8b4441b3a14
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-03-30 16:00:24 -0300

    create array stars

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,15 +1,12 @@
 import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 import { Container } from './styles';
-import { } from 'react';
 
 export function Rate({ rate = 0 }) {
   const starsOuter = [];
   const starsInner = [];
 
-  const key = 10;
-
   for (let i = 0; i < 5; i++) {
-    starsOuter.push(<IoIosStarOutline key={key + i} />);
+    starsOuter.push(<IoIosStarOutline key={i} />);
   }
 
   for (let i = 0; i < rate; i++) {
